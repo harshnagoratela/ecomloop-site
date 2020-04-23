e4588ec7149502ad5bbe44e506020f4656486e5d
Harshna Goratela <harshna.goratela@gmail.com>
2020-04-23 09:56:25 +0530

    22-apr andrew changes

--- src/templates/singleitem - Copy.jsx
+++ src/templates/singleitem - Copy.jsx
@@ -38,14 +38,17 @@ export const query = graphql`
   query($pathSlug: String!) {
     googleSheetListRow(name: {eq: $pathSlug}) {
       name
+      date
       url
       category
       tags
+      amazonlink
       sociallink
       about
       country
       state
       city
+      like
     }
   }
 `;
