d642a01ebb8d10ed0481ad035de0c394a1690569
Harshna Goratela <harshna.goratela@gmail.com>
2020-11-22 11:04:04 +0530

    Andrew 21nov changes:

--- src/utils/algolia.js
+++ src/utils/algolia.js
@@ -4,26 +4,35 @@ const shopQuery = `{
       node {
         id: UniqueID
         random: UniqueKey
+        emprezzoID: UserName
         shopName: name
-        emprezzoID: UniqueID
         url: url
         tags: tags
         about: about
-        category: category
+        shopTags: tags
+        shopCategory: category
         freeShipMin: FreeShipMin
         baseShipRate: BaseShipRate
         returnDays: ReturnDays
         returnShipFree: ReturnShipFree
         priceMin: PriceMin
-        image: ProfilePicURL
+        priceMax: PriceMax
+        priceAvg: PriceAvg
+        productCount: CountProducts
         updateDate: CreateDate
+        shopDescription: Description
+        proudctList: TitleList
+        shopImage: ProfileImage
+        shopImage2: ProfilePicURL
+        trafficRank: GlobalRankOrder
+        socialRankScore: SocialRankScore
       }
     }
   }
 }`
 
 const productQuery = `{
-  products: allMysqlShopifyProductsAll(limit: 1000) {
+  products: allMysqlShopifyProductsAll(limit: 100 ) {
     edges {
       node {
         objectID: UniqueID
@@ -47,7 +56,6 @@ const productQuery = `{
         baseShipRate: BaseShipRate
         returnDays: ReturnDays
         returnShipFree: ReturnShipFree
-        shopImage: ProfilePicURL
         trafficRank: GlobalRankOrder
         socialRankScore: SocialRankScore
       }
