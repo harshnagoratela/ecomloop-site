342447ae70f1d40c60b8ac9e92d9654346f01b9f
Harshna Goratela <harshna.goratela@gmail.com>
2020-12-29 19:22:13 +0530

    29dec changes

--- src/pages/authtest.jsx
+++ src/pages/authtest.jsx
@@ -5,14 +5,16 @@ import { Header } from 'components';
 import { Layout, Container } from 'layouts';
 import Authentication from '../components/Blockstack/Authentication'
 
-const About = center => (
-  <Layout>
-    <Helmet title={'Auth Test'} />
-    <Header title="Auth Test Page">🧐 Discover exceptional retailers & innovative brands<br/>🛒 Shop direct to support independent businesses</Header>
-    <Container center={center}>
-      <Authentication />
-    </Container>
-  </Layout>
-);
+const About = center => {
+  return (
+    <Layout>
+      <Helmet title={'Auth Test'} />
+      <Header title="Auth Test Page">🧐 Discover exceptional retailers & innovative brands<br />🛒 Shop direct to support independent businesses</Header>
+      <Container center={center}>
+        <Authentication />
+      </Container>
+    </Layout>
+  )
+};
 
 export default About;
