942db53eafce7d7f0a23e027ffe36b7f8c027edf
Harshna Goratela <harshna.goratela@gmail.com>
2021-03-02 10:43:42 +0530

    Andrew 01-mar changes

--- src/styles/headroom.js
+++ src/styles/headroom.js
@@ -11,7 +11,9 @@ const headroom = css`
   .css-14za4wp.eqxxxtn1, .css-woc7hc.eqxxxtn0 {
     display: none;
 }
-@import url('https://fonts.googleapis.com/css2?family=Jost:wght@500&family=Overpass+Mono:wght@400&display=swap');
+
+@import url('https://fonts.googleapis.com/css2?family=Jost:wght@400;700&display=swap');
+
 p.center {
     text-align: center;
     margin: 25px;
@@ -56,11 +58,13 @@ button.ais-RefinementList-showMore:hover {
 
 [data-reach-dialog-overlay]{z-index: 10;}
 
-h4 {margin: 1rem; font-size: .8rem;
-font-family: 'Overpass Mono', Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace;}
+h4 {margin: 1rem; font-size: 1rem;
+font-family: 'Jost', sans-serif;
+}
 
-li.ais-Hits-item {
+li.ais-Hits-item, .css-1t9eqyr-AlgoliaProductList--RightPanel .ais-Hits-item {
   border: unset;
+
 }
 
 .lb-ef-bounce {
